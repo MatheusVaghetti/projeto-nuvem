4d26ff36b31c2ff661db4ed7c00f46d4ae4441a4
MatheusVaghetti <146997137+MatheusVaghetti@users.noreply.github.com>
2025-03-28 12:29:51 +0000

    dnv

--- JocaS2/src/main/java/br/dev/JocaS2/JocaS2Application.java
+++ JocaS2/src/main/java/br/dev/JocaS2/JocaS2Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class JocaS2Application {
 
 	public static void main(String[] args) {
